0a1c9cc190e8aae55fd3a3ecc6dcbd45bd295593
Ojas Mittal <ojasfarm31@gmail.com>
2022-06-27 17:34:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
 echo "[theme]
-primaryColor = '#FF4B4B'
-backgroundColor = ‘#f7f7f7’
-secondaryBackgroundColor = ‘#ec5c0e’
-textColor= ‘#101010’
-font = ‘sans-serif’
+primaryColor=’#020202’
+backgroundColor=’#c4c3c3’
+secondaryBackgroundColor=’#ebd316’
+font = ‘sans serif’
 [server]
 headless = true
 port = $PORT
