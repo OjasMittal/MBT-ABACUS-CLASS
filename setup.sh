aef1d0fa9243d4af124a2d09392a71785e67f193
Ojas Mittal <ojasfarm31@gmail.com>
2022-06-27 17:32:05 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ primaryColor = '#FF4B4B'
 backgroundColor = ‘#f7f7f7’
 secondaryBackgroundColor = ‘#ec5c0e’
 textColor= ‘#101010’
-font = ‘sans serif’
+font = ‘sans-serif’
 [server]
 headless = true
 port = $PORT
