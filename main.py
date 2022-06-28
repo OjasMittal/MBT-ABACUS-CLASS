a8d6813ba39945bf8c8e8e9ecc87fc867c7a171e
ojas <ojasfarm31@gmail.com>
2022-06-28 13:40:31 +0530

    major formatting changes

--- main.py
+++ main.py
@@ -1,92 +1,163 @@
 import streamlit as st
 from PIL import Image
+from annotated_text import annotated_text
+import emoji
 
 img=Image.open('icon.jpg')
 st.set_page_config(page_title="MBT ABACUS CLASS",page_icon=img)
-st.sidebar.image(img, caption='MBT ABACUS CLASS',width=250)
+c1,c2,c3=st.columns(3)
+with c2:
+  st.sidebar.image(img, caption='MBT ABACUS CLASS',width=190)
 st.sidebar.title("CONTACT INFORMATION :")
-st.sidebar.subheader("Address:   MBT Abacus Class,Shop No 13,Shri om sadguru chs, Dattatray Tandel Marg, New Sector-50, Seawoods, Navi Mumbai, Maharashtra 400706")
-st.sidebar.subheader("Contact:  9920092640 ~ Tulika Mittal (Center Director)")
-ime = Image.open('fb logo.png')
-st.sidebar.image(ime,
+st.sidebar.write("***Address:***   MBT Abacus Class,Shop No 13,Shri om sadguru chs, Dattatray Tandel Marg, New Sector-50, Seawoods, Navi Mumbai, Maharashtra 400706")
+st.sidebar.write("***Contact:***  9920092640 ~ Tulika Mittal (Center Director)")
+col1,col2,col3=st.sidebar.columns(3)
+with col1:
+    ime = Image.open('fb logo.png')
+    st.image(ime,
              width=40)
-st.sidebar.subheader("Facebook Page: https://rb.gy/rju3lw")
+    ima = Image.open('insta.jpg')
+    st.image(ima,
+             width=40)
+with col2:
+    st.write("**Facebook Page :**")
+    st.write("**Instagram Page :**")
+with col3:
+    st.write('https://rb.gy/rju3lw')
+    st.write('http://surl.li/chprm')
 
 hide_menu_style="""
 <style>
-MainMenu{visibility:hidden;}
+#MainMenu{visibility:hidden;}
 footer{visibility:hidden;}
 </style>
 """
 st.markdown(hide_menu_style,unsafe_allow_html=True)
 
 st.title("MBT ABACUS CLASS \N{trophy}")
-st.subheader(" ")
-st.subheader("MBT Abacus Class was started in the year 2014 by Mrs.Tulika Mittal with a "
-             "hope to provide something good to the society and promote women entrepreneurship.")
-ige = Image.open('mom.jpg')
-st.image(ige, caption='Tulika Mittal',
+st.write("***MBT Abacus Class was started in the year 2014 by Mrs.Tulika Mittal with a "
+             "hope to provide something good to the society and promote women entrepreneurship.***")
+col1,col2=st.columns(2)
+with col1:
+    ige = Image.open('mom_web.jpg')
+    st.image(ige, caption='Tulika Mittal',
              width=300)
-st.subheader("MBT is authorised franchise of Mastermind and WriteRight India and is known to provide quality education with various kinds of courses like Abacus,"
-             "Handwriting improvement,Speed Writing,Calligraphy and Guitar.")
+with col2:
+    ige = Image.open('class.png')
+    st.image(ige, caption='Seawoods Center',
+             width=400)
+st.write("MBT is authorised franchise of **Mastermind** and **WriteRight-India** and is known to provide quality education with various kinds of courses like **Abacus,"
+             "Handwriting improvement,Speed Writing,Calligraphy and Guitar.**")
 image = Image.open('class.jpg')
 st.image(image, caption='MBT ABACUS CLASS,SEAWOODS,NAVI MUMBAI',
-             width=500)
+             width=400)
 st.title("Abacus")
 image1 = Image.open('mastermindabacus-logo.png')
 st.image(image1, caption='Mastermind Abacus',
              width=400)
-st.subheader("Finding a quality Abacus academy for a child is not easy, as parents are not"
-             " aware of the intricacies of Abacus training & learning.MBT is the Navi Mumbai Master Franchise of Mastermind Abacus.")
-imge2 = Image.open('benifits.jpg')
-st.image(imge2,width=300)
-st.subheader("When a child learns Abacus, parents expect improvement in their mental "
+st.write("**Finding a quality Abacus academy for a child is not easy, as parents are not"
+             " aware of the intricacies of Abacus training & learning.MBT is the Navi Mumbai Master Franchise of Mastermind Abacus.**")
+col1,col2=st.columns(2)
+with col1:
+    imge2 = Image.open('benifits.jpg')
+    st.image(imge2,width=300)
+with col2:
+    imge2 = Image.open('abacus1.jpg')
+    st.image(imge2, width=400)
+st.write("**When a child learns Abacus, parents expect improvement in their mental "
              "math abilities. In addition, they expect enhanced listening ability, visualization capacity, and "
-             "concentration levels in their children.")
-st.subheader("They expect whole-brain development and increased confidence "
-             "in the child. Also, overall performance in Academics should reflect their improved abilities")
-imge1 = Image.open('ab.jpg')
-st.image(imge1,width=400)
-st.subheader(" Furthermore, Mastermind's game-based program ensures the learning process does not become monotonous, "
-             "which is very important for the expected results.")
+             "concentration levels in their children.**")
+col1,col2=st.columns(2)
+with col1:
+  imge1 = Image.open('ab.jpg')
+  st.image(imge1,width=300)
+with col2:
+  imge1 = Image.open('images.jpg')
+  st.image(imge1, width=350)
 
-st.video('https://youtu.be/6eFyspBqvNE')
-st.subheader("Age  Criteria : 5 to 14 years")
-st.subheader("Duration : 8 levels of 3 months each and 2 advanced levels  with once-in-a-week classes of two hours.")
-st.subheader("Method : Fun & Learn Method with special speed building software mobile app")
+col1,col2=st.columns(2)
+with col1:
+    imge1 = Image.open('images (1).jpg')
+    st.image(imge1, width=300)
 
+with col2:
+    imge1 = Image.open('img3.jpg')
+    st.image(imge1, width=350)
+
+
+st.write("**They expect whole-brain development and increased confidence "
+             "in the child. Also, overall performance in Academics should reflect their improved abilities**")
+
+
+col1,col2,col3=st.columns(3)
+with col1:
+    st.video('https://youtu.be/6eFyspBqvNE')
+with col2:
+    st.video('https://www.youtube.com/watch?v=Gu1zvqtXJzA')
+with col3:
+    st.video('https://www.youtube.com/watch?v=9GoGHxOjGtA')
+col1,col2,col3=st.columns(3)
+with col1:
+    st.video('https://www.youtube.com/watch?v=a9BnNWrzHY4')
+with col2:
+    st.video('https://www.youtube.com/watch?v=1grEKIqDDVA')
+with col3:
+    st.video('https://www.youtube.com/watch?v=VbG92I6cp1Q')
+st.write(
+        " ***Furthermore, Mastermind's game-based program ensures the learning process does not become monotonous, "
+        "which is very important for the expected results.***")
+annotated_text(("Age  Criteria ", ":","#8ef"),("5-14 years"," ","#faa"))
+st.write(" ")
+annotated_text(("Duration", ":","#8ef"),("8 levels of 3 months each and 2 advanced levels  with once-in-a-week classes of two hours."," ","#faa"))
+st.write(" ")
+annotated_text(("Method", ":","#8ef"),("Fun & Learn Method with special speed building software mobile app"," ","#faa"))
+st.write(" ")
 st.subheader("Kindly fill the form below for further discussion and enrollment.")
 st.info("https://forms.gle/cSgXzbG77TrdDp1B9")
 st.title("Handwriting Improvement")
 image2 = Image.open('write right.png')
 st.image(image2, caption='Write Right India',
              width=400)
-st.subheader("Handwriting Can Be Improved At Any Age!")
-st.subheader("But, the fact is that Handwriting invariably, can be corrected at any age."
-             " The only condition applied is that the writer should have steady hands.")
-st.subheader("MBT is an authorized franchise of Write Right India and offers 3 types of handwriting courses:")
-st.subheader("1.Handwriting Improvement-Improve your handwriting within 7 days with 100% money back guarantee")
-st.subheader("2.Calligraphy-Learn various fonts of Calligraphy and score full marks in your projects just in 15 days!!")
-st.subheader("3.Speed Writing- Loosing marks in exams due to lack of time?"
-             " Dont worry, just improve your writing speed with our certified "
-             "course of speed writing and complete your exam faster than others!! :smile:")
-image3 = Image.open('handwr.jpg')
-st.image(image3, caption='100% Money Back Challenge',
-             width=400)
-image4=Image.open('handwriting.jpg')
-st.image(image4, caption='Gift yourself a beautiful handwriting!',
-             width=400)
+st.header("Handwriting Can Be Improved At Any Age!")
+st.write("**But, the fact is that Handwriting invariably, can be corrected at any age."
+             " The only condition applied is that the writer should have steady hands.**")
+st.write("***MBT is an authorized franchise of Write Right India and offers 3 types of handwriting courses:***")
+annotated_text(("1.Handwriting Improvement",":","#8ef"),("Improve your handwriting within 7 days with 100% money back guarantee"," ","#faa"))
+st.write(" ")
+annotated_text(("2.Calligraphy",":","#8ef"),("Learn various fonts of Calligraphy and score full marks in your projects just in 15 days!!"," ","#faa"))
+st.write(" ")
+annotated_text(("3.Speed Writing",":","#8ef"),("Loosing marks in exams due to lack of time?  Dont worry, just improve your writing speed with our certified "
+             "course of speed writing and complete your exam faster than others!!"," ","#faa"))
+st.write(" ")
+st.header(" ")
+col1,col2=st.columns(2)
+with col1:
+  image3 = Image.open('handwr.jpg')
+  st.image(image3, caption='100% Money Back Challenge',
+             width=350)
+with col2:
+  image4=Image.open('handwriting.jpg')
+  st.image(image4, caption='Gift yourself a beautiful handwriting!',
+             width=350)
 st.subheader("Kindly fill the form below for further discussion and enrollment.")
 st.info("https://forms.gle/cSgXzbG77TrdDp1B9")
 st.title("Guitar")
 image5=Image.open('guitar.jpg')
 st.image(image5, caption='Enhance your skills!',
              width=300)
-st.subheader("MBT has tied up with Plexus Academy to give best quality guitar lessons to students.Classes occurs every Wednesday from 7-8:30 pm")
+st.write("**MBT has tied up with Plexus Academy to provide best quality guitar lessons.Classes occurs every-**")
+annotated_text(("Wednesday","from",'#8ef'),("7:00-8:30","pm","#faa"))
 st.subheader("Kindly fill the form below for further discussion and enrollment.")
 st.info("https://forms.gle/cSgXzbG77TrdDp1B9")
-st.subheader(" Thanks for Visiting!")
-st.info('This Website has been developed by a MBT Alumini. \N{slightly smiling face}')
+st.subheader("Thanks for Visiting!")
+st.info('This Website has been developed by an MBT Alumini. \N{slightly smiling face}')
+col1,col2,col3,col4,col5,c6,c7,c8,c9,c10=st.columns(10)
+with col1:
+    if st.button(emoji.emojize(':thumbs_up:',use_aliases=True)):
+        st.balloons()
+with col2:
+    if st.button(emoji.emojize(':thumbs_down:',use_aliases=True)):
+        st.snow()
 
 
 
